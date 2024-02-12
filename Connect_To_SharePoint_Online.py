009081052764951fd91a3228810aeee1d3657156
Subash Mishra <51420981+MishraSubash@users.noreply.github.com>
2024-02-12 11:03:15 -0600

    finalize code

--- Connect_To_SharePoint_Online.py
+++ Connect_To_SharePoint_Online.py
@@ -6,11 +6,12 @@ from office365.sharepoint.files.file import File
 
 
 # SharePoint Credentials
-sharepoint_clientId = "XXXXXXXXXXXXXXXXXX"
-sharepoint_clientsecret = "XXXXXXXXXXXXXXXXXXXXXXXX"
+sharepoint_clientId = "XXXXXXXXXXXXXXXX"
+sharepoint_clientsecret = "XXXXXXXXXXXXXXXXXXXXXXX"
 
 
 class SharePoint_Connection:
+
     def __init__(self, client_id: str, client_secret: str, team: str) -> None:
         """
         Constructor to initialize SharePoint_Connection object with client_id, client_secret, and team values.
@@ -40,19 +41,25 @@ class SharePoint_Connection:
             If an error occurs during the establishment of the SharePoint context, an exception is caught and an error message
             is printed, and the function returns None.
         """
-        # Establishes a SharePoint context using the provided client_id, client_secret, and team.
+
         try:
-            site_url = f"https://YOUR_COMPANY.sharepoint.com/teams/{self.team}"
+            # SharePoint site URL based on the company's domain name and team
+            site_url = f"https://<example>.sharepoint.com/teams/{self.team}"
+            # Authentication context using client_id and client_secret
             context_auth = AuthenticationContext(site_url)
+            # Acquire token for the application
             if context_auth.acquire_token_for_app(
                 client_id=self.client_id, client_secret=self.client_secret
             ):
+                # Create SharePoint client context
                 ctx = ClientContext(site_url, context_auth)
                 return ctx
         except Exception as e:
+            # Print error message if an exception occurs during SharePoint context establishment
             print(f"Error: {type(e).__name__} {e}")
+            return None
 
-    def create_sharepoint_directory(self, directory_name: str) -> None:
+    def create_sharepoint_directory(self, directory_name: str) -> str | None:
         """
         Creates a directory in SharePoint under the 'Shared Documents/General/' path.
 
@@ -63,22 +70,34 @@ class SharePoint_Connection:
             - str: The relative URL of the created directory if successful. If an error occurs during the creation process,
             an error message is printed, and the function returns an empty string.
         """
+
         if directory_name:
             # Establish SharePoint context
             ctx = self.establish_sharepoint_context()
-            # Attempt to create the directory
 
-            result = ctx.web.folders.add(
-                f"Shared Documents/General/{directory_name}"
-            ).execute_query()
-            # If successful, return the relative URL of the created directory
-            if result:
-                relative_url = f"Shared Documents/General/{directory_name}"
-                print(relative_url)
-                return relative_url
-            else:
-                print("Fail to create a folder/directory!")
+            # Attempt to create the directory
+            try:
+                result = ctx.web.folders.add(
+                    f"Shared Documents/General/{directory_name}"
+                ).execute_query()
+                # If successful, return the relative URL of the created directory
+                if result:
+
+                    relative_url = f"Shared Documents/General/{directory_name}"
+                    print(
+                        f"{directory_name} directory has been created at '{relative_url}'"
+                    )
+                    return relative_url
+                else:
+                    print("Failed to create a folder/directory!")
+                    return ""
+            except Exception as e:
+                # Print error message if an exception occurs during directory creation
+                print(f"Error: {type(e).__name__} {e}")
                 return ""
+        else:
+            print("Directory name cannot be empty!")
+            return ""
 
     def read_sharepoint_file_as_df(self, file_path: str, dtype=None) -> pd.DataFrame:
         """
@@ -92,7 +111,7 @@ class SharePoint_Connection:
             - pd.DataFrame: A Pandas DataFrame containing the content of the specified file. If the 'dtype' parameter is provided,
             it is used to specify data types for DataFrame columns during reading.
         """
-        # # Establish SharePoint context
+        # Establish SharePoint context
         ctx = self.establish_sharepoint_context()
         web = ctx.web
         ctx.load(web)
@@ -106,7 +125,7 @@ class SharePoint_Connection:
         )
         # Read file content into Pandas DataFrame
         if dtype is not None:
-            # # If data types are specified, use them during DataFrame creation
+            # If data types are specified, use them during DataFrame creation
             # df = pd.read_csv(out, dtype=dtype)
             df = pd.read_excel(out, dtype=dtype)
         else:
@@ -118,10 +137,7 @@ class SharePoint_Connection:
         return df
 
     def write_bytefile_to_sharepoint(
-        self,
-        file_path: str,
-        file_name: str,
-        file_bytes: bytes,
+        self, file_path: str, file_name: str, file_bytes: bytes
     ) -> None:
         """
         Writes a byte file to SharePoint in the specified folder with the given file name.
@@ -134,13 +150,13 @@ class SharePoint_Connection:
         Returns:
             - None
         """
-        # # Establish SharePoint context
+        # Establish SharePoint context
         ctx = self.establish_sharepoint_context()
         # Get the SharePoint folder by server-relative URL
         folder: Folder = ctx.web.get_folder_by_server_relative_url(
             f"Shared Documents/{file_path}"
         )
-        # # Chunk size for uploading
+        # Chunk size for uploading
         chunk_size: int = 500000
 
         # Check if the file already exists
@@ -161,31 +177,34 @@ class SharePoint_Connection:
                 file=file_to_upload, chunk_size=chunk_size, file_name=file_name
             ).execute_query()
         # Print success message
-        print(f"File has been uploaded successfully!")
+        print(f"{file_name} has been uploaded successfully!")
 
 
+# Establish Connection
 connectionString = SharePoint_Connection(
     client_id=sharepoint_clientId,
     client_secret=sharepoint_clientsecret,
-    team="[Your SharePoint Team's Name]",
+    team="Your Company's Team Name",
 )
 
+########################################################################
+# Create directory
+connectionString.create_sharepoint_directory("folder_name")
 
-# create directory
-# connectionString.create_sharepoint_directory("TestFolder")
+#########################################################################
 
+# Read sharepoint files
+df = connectionString.read_sharepoint_file_as_df(
+    "General/folder/file_name.xlsx",
+)
+print(df.head())
 
-# # read sharepoint excel file
-# df = connectionString.read_sharepoint_file_as_df(
-#     "General/folder/file_name.xlsx",
-# )
-# print(df.head())
-
-# create pandas data frame to test upload
+########################################################################
+# Write files to SharePoint Online
 df = pd.DataFrame(data={"col1": [1, 2], "col2": [3, 4], "col3": [5, 6]})
 fileBytes = bytes(df.to_csv(index=False), encoding="utf-8")
 
 
 connectionString.write_bytefile_to_sharepoint(
-    "General/folder_root/sub_folder", "test2.csv", fileBytes
+    "General/folder", "file_name.csv", fileBytes
 )
